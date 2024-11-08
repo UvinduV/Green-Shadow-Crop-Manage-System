e370a91c9c55da37e0c59165edd13fe20d4caecb
UvinduV <uvindusanketh470@gmail.com>
2024-11-08 09:28:35 +0530

    add to git

---
+++ src/main/java/lk/ijse/green_shadow_crop_manage_system/GreenShadowCropManageSystemApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.green_shadow_crop_manage_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenShadowCropManageSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenShadowCropManageSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/ijse/green_shadow_crop_manage_system/GreenShadowCropManageSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.green_shadow_crop_manage_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenShadowCropManageSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
