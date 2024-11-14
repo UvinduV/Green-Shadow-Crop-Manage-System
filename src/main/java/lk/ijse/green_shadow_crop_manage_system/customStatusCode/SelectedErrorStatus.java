cff8f8cb6fb9a02fc74cf06ac0209c53535e580c
UvinduV <uvindusanketh470@gmail.com>
2024-11-14 15:37:59 +0530

    create custom status

---
+++ src/main/java/lk/ijse/green_shadow_crop_manage_system/customStatusCode/SelectedErrorStatus.java
@@ -0,0 +1,14 @@
+package lk.ijse.green_shadow_crop_manage_system.customStatusCode;
+
+import lk.ijse.green_shadow_crop_manage_system.dto.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class SelectedErrorStatus implements UserStatus, StaffStatus, FieldStatus, CropStatus, VehicleStatus,EquipmentStatus,MonitoringLogStatus {
+    private int statusCode;
+    private String statusMassage;
+}
