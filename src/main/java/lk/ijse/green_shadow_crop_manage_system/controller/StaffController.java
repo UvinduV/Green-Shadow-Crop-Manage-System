a42fca88308a2ac90bd876d69337b8bc9fc9634e
UvinduV <uvindusanketh470@gmail.com>
2024-11-14 14:00:28 +0530

    add mapping class

---
+++ src/main/java/lk/ijse/green_shadow_crop_manage_system/controller/StaffController.java
@@ -0,0 +1,9 @@
+package lk.ijse.green_shadow_crop_manage_system.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/staff")
+public class StaffController {
+}
---
+++ src/main/java/lk/ijse/green_shadow_crop_manage_system/util/Mapping.java
@@ -0,0 +1,17 @@
+package lk.ijse.green_shadow_crop_manage_system.util;
+
+import lk.ijse.green_shadow_crop_manage_system.dto.Impl.StaffDTO;
+import lk.ijse.green_shadow_crop_manage_system.entity.Impl.StaffEntity;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Mapping {
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public StaffEntity toStaffEntity(StaffDTO staffDTO){
+        return modelMapper.map(staffDTO, StaffEntity.class);
+    }
+}
