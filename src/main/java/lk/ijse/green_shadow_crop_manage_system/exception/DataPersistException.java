84eae526ebf7eecb59afedc501b7828db44b8abd
UvinduV <uvindusanketh470@gmail.com>
2024-11-14 14:10:29 +0530

    create exception for staff

---
+++ src/main/java/lk/ijse/green_shadow_crop_manage_system/exception/DataPersistException.java
@@ -0,0 +1,10 @@
+package lk.ijse.green_shadow_crop_manage_system.exception;
+
+public class DataPersistException extends RuntimeException{
+    public DataPersistException() {
+    }
+    public DataPersistException(String message) {
+    }
+    public DataPersistException(String message, Throwable cause) {
+    }
+}
---
+++ src/main/java/lk/ijse/green_shadow_crop_manage_system/exception/StaffNotFoundException.java
@@ -0,0 +1,10 @@
+package lk.ijse.green_shadow_crop_manage_system.exception;
+
+public class StaffNotFoundException extends RuntimeException{
+    public StaffNotFoundException() {
+    }
+    public StaffNotFoundException(String message) {
+    }
+    public StaffNotFoundException(String message, Throwable cause) {
+    }
+}
