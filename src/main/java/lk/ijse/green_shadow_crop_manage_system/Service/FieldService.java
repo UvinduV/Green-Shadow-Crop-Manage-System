8aaeac73d76013c28594fe80efeac628bc6c5be8
UvinduV <uvindusanketh470@gmail.com>
2024-11-15 13:45:04 +0530

    create field service

---
+++ src/main/java/lk/ijse/green_shadow_crop_manage_system/Service/FieldService.java
@@ -0,0 +1,4 @@
+package lk.ijse.green_shadow_crop_manage_system.Service;
+
+public interface FieldService {
+}
---
+++ src/main/java/lk/ijse/green_shadow_crop_manage_system/Service/Impl/FieldServiceImpl.java
@@ -0,0 +1,10 @@
+package lk.ijse.green_shadow_crop_manage_system.Service.Impl;
+
+import jakarta.transaction.Transactional;
+import lk.ijse.green_shadow_crop_manage_system.Service.FieldService;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class FieldServiceImpl implements FieldService {
+}
