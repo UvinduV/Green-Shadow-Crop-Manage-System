b83dad7f2623c8b6dab2c7a5f2f5b61756646567
UvinduV <uvindusanketh470@gmail.com>
2024-11-29 13:02:57 +0530

    refactor save vehicle

--- src/main/java/lk/ijse/green_shadow_crop_manage_system/controller/VehicleController.java
+++ src/main/java/lk/ijse/green_shadow_crop_manage_system/controller/VehicleController.java
@@ -8,21 +8,21 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.awt.*;
-
 @RestController
-@RequestMapping("api/v1/vehicles")
+@RequestMapping("api/v1/vehicle")
 public class VehicleController {
     @Autowired
     private VehicleService vehicleService;
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Void> saveVehicle(VehicleDTO vehicleDTO) {
+    public ResponseEntity<Void> saveVehicle(@RequestBody VehicleDTO vehicleDTO) {
         try {
+            System.out.println(vehicleDTO);
             vehicleService.saveVehicle(vehicleDTO);
             return new ResponseEntity<>(HttpStatus.CREATED);
         }catch (DataPersistException e){
